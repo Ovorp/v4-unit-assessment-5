11805808a1b98543b43e760c1fd22027aa2fa1f2
Nathaniel Shirts <nshirts7@gmail.com>
2021-05-26 23:52:32 -0600

    finish the routing section

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -13,52 +13,48 @@ class Post extends Component {
       title: '',
       img: '',
       content: '',
-      loading: true
-    }
+      loading: true,
+    };
   }
 
   componentDidMount() {
-    axios.get(`/api/post/1`)
-      .then(res => {
-        this.setState({ ...res.data, loading: false })
-      })
+    axios.get(`/api/post/${this.props.match.params}`).then((res) => {
+      this.setState({ ...res.data, loading: false });
+    });
   }
 
   render() {
     let imgSrc = this.state.img ? this.state.img : noImage;
 
     return (
-      <div className='post content-box'>
-        {!this.state.loading && this.state.title
-          ?
+      <div className="post content-box">
+        {!this.state.loading && this.state.title ? (
           <div>
-            <div className='post-header'>
-              <h2 className='title'>{this.state.title}</h2>
-              <div className='author-box'>
+            <div className="post-header">
+              <h2 className="title">{this.state.title}</h2>
+              <div className="author-box">
                 <p>by {this.state.author}</p>
-                <img src={this.state.author_pic} alt='author' />
+                <img src={this.state.author_pic} alt="author" />
               </div>
             </div>
-            <div className='post-content-box'>
-              <img className='post-img' src={imgSrc} alt='post' />
+            <div className="post-content-box">
+              <img className="post-img" src={imgSrc} alt="post" />
               <p>{this.state.content}</p>
             </div>
           </div>
-          :
-          !this.state.loading
-            ?
-            <div className='oops-box'>
-              <h2 className='title'>Oops!</h2>
-              <p>Looks like this post doesn't exist anymore</p>
-            </div>
-            :
-            <div className='load-box'>
-              <div className='load-background'></div>
-              <div className='load'></div>
-            </div>
-        }
+        ) : !this.state.loading ? (
+          <div className="oops-box">
+            <h2 className="title">Oops!</h2>
+            <p>Looks like this post doesn't exist anymore</p>
+          </div>
+        ) : (
+          <div className="load-box">
+            <div className="load-background"></div>
+            <div className="load"></div>
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
 
